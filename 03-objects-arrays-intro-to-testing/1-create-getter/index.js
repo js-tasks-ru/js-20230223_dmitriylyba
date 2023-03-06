ca9205cb49b6a459dbce08cf7d03113043c4a643
Voanarges <dmitriylyba@gmail.com>
2023-03-06 15:05:54 +0300

    module 3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,28 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  let way
+  if (path.split('.') < 1) {
+    way = path
+  } else {
+    way = path.split('.').at(-1)
+  }
 
+  return function (obj) {
+    const arr = new Map(Object.entries(obj))
+
+    const check = [...arr.values()][0]
+    if (check[way]) {
+      return check[way]
+    }
+
+    return Object.entries([...arr.values()][0].at(-1))
+  }
 }
+
+// const obj4 = { more: { nested: { property: 1 } } }
+// const obj2 = { nested: { property: 1 } }
+// const getter = createGetter('nested.property')
+// const getter = createGetter('more.nested.property')
+// console.log(getter(obj4))
+// node ./03-objects-arrays-intro-to-testing/1-create-getter/index.js
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const arr = string.split('')
+  const set = new Set(arr)
+  const map = new Map(Object.entries(arr))
+  let result
+  if (size < 1) {
+    result = ''
+  }
+  if (size < 2) {
+    console.log(size < 2)
+    result = [...set].join('')
+  }
+  if (size > 1) {
+    // не решенное условие
+    // for (let i = 0; size; i++) {}
+    // result = map.values()
+    result = string
+  }
 
+  return result
 }
