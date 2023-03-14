b1dbed60f903873a4055842e9c72b24f09dc58e1
Voanarges <dmitriylyba@gmail.com>
2023-03-14 06:59:00 +0300

    добавил себе вопросы комментарии

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,7 +4,7 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export const trimSymbols = (string, size) => {
+const trimSymbols = (string, size) => {
   if (size === 0) {
     return ''
   }
@@ -16,9 +16,14 @@ export const trimSymbols = (string, size) => {
   const rest = [...string.slice(size)]
 
   return rest.reduce((accumString, char) => {
+    console.log('firstSlice', firstSlice)
+    // console.log('repeat', char.repeat(size))
     if (!accumString.endsWith(char.repeat(size))) {
       accumString += char
     }
     return accumString
   }, firstSlice)
 }
+
+console.log(trimSymbols('xxxaaaaab', 1))
+// node ./03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
