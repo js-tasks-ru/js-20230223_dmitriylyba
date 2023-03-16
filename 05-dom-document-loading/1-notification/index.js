9db954ae819360b02a20d2174ee98d74145228b0
Voanarges <dmitriylyba@gmail.com>
2023-03-16 10:08:40 +0300

    completed task 4.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+  static shownMessage
+  timer
+  constructor(message = '', { duration = 1000, type = 'success' } = {}) {
+    this.message = message
+    this.duration = duration
+    this.durationInSec = duration / 1000
+    this.type = type
 
+    this.render()
+  }
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationInSec}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  render() {
+    const element = document.createElement('div')
+    element.innerHTML = this.template
+    this.element = element.firstElementChild
+  }
+  show(main = document.body) {
+    console.dir(NotificationMessage)
+    if (NotificationMessage.shownMessage) {
+      NotificationMessage.shownMessage.remove()
+    }
+    main.append(this.element)
+
+    this.timer = setTimeout(() => {
+      this.remove()
+    }, this.duration)
+
+    NotificationMessage.shownMessage = this
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove()
+    }
+  }
+
+  destroy() {
+    this.remove()
+    this.element = null
+    NotificationMessage.shownMessage = {}
+  }
 }
